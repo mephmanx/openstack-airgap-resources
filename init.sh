2ab419a004c78da59917bd22b18f8054641e8799
Chris Lyons <chris@lyonsgroup.family>
2022-02-09 18:29:26 -0500

    Initial version

--- init.sh
+++ init.sh
@@ -58,11 +58,11 @@ cat /root/all_rpms_w.txt |sort |sort -u >/root/w_rpm_list.txt
 
 docker run --rm -u root -v /root/:/root/ -v /var/run/docker.sock:/var/run/docker.sock  rpm_repo/kolla/centos-binary-base:wallaby bash -c "rpm -qa >/root/base_rpm.txt"
 
-cat w_rpm_list.txt base_rpm.txt |sort |uniq -u >to_be_download_w.txt
+cat /root/w_rpm_list.txt /root/base_rpm.txt |sort |uniq -u >to_be_download_w.txt
 
 mkdir -p /root/kolla_wallaby
 
-docker run -u root -v /root/:/root/ -v /var/run/docker.sock:/var/run/docker.sock --rm  rpm_repo/kolla/centos-binary-base:wallaby bash -c "/root/download_rpm.sh"
+docker run -u root -v /root/:/root/ -v /var/run/docker.sock:/var/run/docker.sock --rm  rpm_repo/kolla/centos-binary-base:wallaby bash -c "/root/download_rpms.sh"
 #create local rpm repo
 createrepo /root/kolla_wallaby/
 cd /root/kolla_wallaby && repo2module -s stable  . modules.yaml && modifyrepo_c --mdtype=modules modules.yaml repodata/
